51a2fe75ce0f7c3bc842181f53a59b145020e7f1
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-01 22:47:07 +0100

    prova 22.47

---
+++ dadi.js
@@ -0,0 +1,24 @@
+
+//const mynumb = parseInt(prompt("put your number"));
+//const numericasuali = Math.floor(Math.random() * 6) ;
+//const numericasualiValue = numericasuali.value
+//
+rndNumb.addEventListener("click" , function() {
+    
+    const num1 = Math.round(Math.random()*5+1);
+    console.log(num1);      
+})
+rndNumb2.addEventListener("click" , function(){
+    const num2 = Math.round(Math.random()*5+1);
+    console.log(num2);
+})
+
+if(num1 > num2) {
+    console.log("hai vinto");
+    }else if (num1 < num2){
+        console.log("hai perso");
+    }else if (num1 = num2) {
+        console.log("pareggio");
+    }else{
+        console.log("error");
+    }
--- main.js
+++ main.js
@@ -1,31 +1,54 @@
 const usersMail =["francesca@icloud.com" , "sara@icloud.com" , "mariella@icloud.com" , "francesco@icloud.com"];
-//let  = usersMail.every(i => usersMail.includes(i));
-//console.log();
-//let user2 = usersMail.every(i => usersMail.includes(i));
-//console.log(user2);
-//let user3 = usersMail.every(i => usersMail.includes(i));
-//console.log(user3);
-//let user4 = usersMail.every(i => usersMail.includes(i));
-//console.log(user4);
-let user = usersMail.every(i=> usersMail.includes("email"))
-console.log(user);
-//const IncludesD = usersMail.includes('email');
-//console.log(IncludesD , 'nontrovato'); 
+//let user = usersMail.every(i=> usersMail.includes("email"))
+//console.log(user);
+
+const frst = ("francesca@icloud.com")
 
 const userMail = document.getElementById("mail")
 const btn = document.getElementById("share");
 
+/*btn.addEventListener("click" , function() {
+    const email = userMail.value;
+    console.log(usersMail[email]);
+})        
+for (let i = 0 ; i < usersMail.length ; i++) {
+    if ( email == true) {
+        console.log("utente trovato") ; 
+    } else (email == false) 
+         console.log("registrati");  
+}*/
+
+/*for (let i = 0 ; i < usersMail.length ; i++) {
 btn.addEventListener("click" , function() {
     const email = userMail.value;
-        //console.log(email);
-    for (let i = 0 ; i < usersMail.length ; i++) {
-        if ( user == true) {
+        console.log(usersMail[email]);
+    
+        if ( email == true) {
             console.log("utente trovato") ; 
-        } else (user == false) 
-            console.log("registrati");
-        
-    }  
+        } else (email == false) 
+             console.log("registrati");  
 })
+}*/
+btn.addEventListener("click" , function(){
+    const email = userMail.value;
+    console.log(usersMail[email]);
+    for (let i = 0 ; i < usersMail.length ; i++){
+        if(email == true) {
+            console.log("utente trovato");
+        } else if(email == false){ 
+            console.log("registrati");
+        }
+    }
+})
+
+/*if(usersMail.indexOf("frst") !== -1){
+    alert("Yes, the value exists!");  
+}   
+else  
+{  
+    alert("No, the value is absent.");  
+}*/  
+
 
 
 
